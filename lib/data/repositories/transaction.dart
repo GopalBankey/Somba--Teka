3589602eb22d3ba63d49614fe34916a81141ed01
GopalBankey <gopal.gmtechnosys@gmail.com>
2025-08-05 10:31:46 +0530

    Gopal Push

---
+++ lib/data/repositories/transaction.dart
@@ -0,0 +1,22 @@
+import 'package:SombaTeka/data/model/data_output.dart';
+import 'package:SombaTeka/data/model/transaction_model.dart';
+import 'package:SombaTeka/utils/api.dart';
+
+class TransactionRepository {
+  Future<DataOutput<TransactionModel>> fetchTransactions(
+      {required int page}) async {
+    Map<String, dynamic> parameters = {
+      Api.page: page,
+    };
+
+    Map<String, dynamic> response = await Api.get(
+        url: Api.getPaymentDetailsApi, queryParameters: parameters);
+
+    List<TransactionModel> transactionList = (response['data']['data'] as List)
+        .map((e) => TransactionModel.fromJson(e))
+        .toList();
+
+    return DataOutput<TransactionModel>(
+        total: response['data']['total'] ?? 0, modelList: transactionList);
+  }
+}
---
+++ lib/ui/screens/home/widgets/location_widget.dart
@@ -0,0 +1,61 @@
+import 'package:SombaTeka/app/routes.dart';
+import 'package:SombaTeka/ui/screens/location/locationHelperWidget.dart';
+import 'package:SombaTeka/ui/theme/theme.dart';
+import 'package:SombaTeka/utils/app_icon.dart';
+import 'package:SombaTeka/utils/custom_text.dart';
+import 'package:SombaTeka/utils/extensions/extensions.dart';
+import 'package:SombaTeka/utils/hive_keys.dart';
+import 'package:SombaTeka/utils/ui_utils.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+class LocationWidget extends StatelessWidget {
+  const LocationWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(spacing: 10,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        GestureDetector(
+          onTap: () async {
+            Navigator.pushNamed(context, Routes.countriesScreen,
+                arguments: {"from": "home"});
+          },
+          child: Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+                color: context.color.secondaryColor,
+                borderRadius: BorderRadius.circular(10)),
+            child: UiUtils.getSvg(
+              AppIcons.location,
+              fit: BoxFit.none,
+              color: context.color.territoryColor,
+            ),
+          ),
+        ),
+
+        Expanded(
+          child: ValueListenableBuilder(
+              valueListenable: Hive.box(HiveKeys.userDetailsBox).listenable(),
+              builder: (context, value, child) {
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CustomText(
+                      "locationLbl".translate(context),
+                      color: context.color.textColorDark,
+                      fontSize: context.font.small,
+                    ),
+                    LocationHelperWidget().selectedDefaultLocation(),
+                  ],
+                );
+              }),
+        ),
+      SizedBox(width: 10,)
+      ],
+    );
+  }
+}
